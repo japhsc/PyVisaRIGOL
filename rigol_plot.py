ccb1fd3ffc37b6377f35e9555b5d269199f3e463
Jan-Philipp Schröder <jan.philipp.schroeder@physik.uni-freiburg.de>
2018-04-23 11:54:46 +0200

    clean up

--- rigol_plot.py
+++ rigol_plot.py
@@ -13,13 +13,16 @@ by Cibo Mahto.
 
 import numpy
 import visa
+import os
 
 class ResourceManager(object):
     def __init__(self, **kwargs):
-        self.rm = visa.ResourceManager(**kwargs)
-        #self.rm = visa.ResourceManager('@py',**kwargs)
-        #self.rm = visa.ResourceManager('@ni',**kwargs)
-
+        if os.name == 'posix':
+            self.rm = visa.ResourceManager('@py',**kwargs)
+        elif os.name == 'nt':
+            self.rm = visa.ResourceManager('@ni',**kwargs)
+        else:
+            self.rm = visa.ResourceManager(**kwargs)
     def __enter__(self):
         return(self.rm)
 
@@ -29,18 +32,41 @@ class ResourceManager(object):
 class Instrument(object):
     def __init__(self, rm, resource_name, **kwargs):
         self.inst = rm.open_resource(resource_name, **kwargs)
-
+        #self.inst.encoding =  "latin-1"
     def __enter__(self):
-        return(self.inst)
-
+        #return(self.inst)
+        return(self)
     def __exit__(self, exc_type, exc, exc_tb):
         self.inst.close()
+    
+    def query(self,command):
+        #return(self.inst.query(command))
+        #return(self.inst.query_binary_values(command, datatype='b', is_big_endian=True))
+        self.write(command) #Request the data
+        rawdata = self.read_raw()
+        data = rawdata
+        while (data[-1] is not 10):
+            data =  data[:-1]
+        while (data[-1] is 10):
+            data =  data[:-1]
+        #print(data)
+        return(data.decode("utf-8"))
         
+    def read_raw(self):
+        return(self.inst.read_raw()) #Read the block of data
+    
+    def write(self,command):
+        return(self.inst.write(command))
+
 
 def list_device():
-    rm = visa.ResourceManager()
-    #rm = visa.ResourceManager('@py')
-    #rm = visa.ResourceManager('@ni')
+    if os.name == 'posix':
+        rm = visa.ResourceManager('@py')
+    elif os.name == 'nt':
+        rm = visa.ResourceManager('@ni')
+    else:
+        rm = visa.ResourceManager()
+
     # Get the USB device, e.g. 'USB0::0x1AB1::0x0588::DS1ED141904883'
     instruments = rm.list_resources()
     usb = list(filter(lambda x: 'USB' in x, instruments))
@@ -48,11 +74,17 @@ def list_device():
 
 
 def read(adr,channel=1):
-    rm = visa.ResourceManager()
-    #rm = visa.ResourceManager('@py')
-    #rm = visa.ResourceManager('@ni')
-    #with Instrument(rm, adr, timeout=2000, chunk_size=1024000) as scope:
-    with Instrument(rm, adr) as scope:
+    if os.name == 'posix':
+        rm = visa.ResourceManager('@py')
+    elif os.name == 'nt':
+        rm = visa.ResourceManager('@ni')
+    else:
+        rm = visa.ResourceManager()
+        
+    with Instrument(rm, adr, timeout=2000, chunk_size=1024000) as scope:
+    #with Instrument(rm, adr) as scope:
+        #print(scope.query("*IDN?"))
+        
         scope.write(':KEY:LOCK')
         scope.write(':RUN')
         scope.write(':ACQuire:MDEPth 600000')
@@ -64,73 +96,73 @@ def read(adr,channel=1):
         # Grab the raw data from channel 1
         #scope.write(u':STOP\n')
         
-        mem_depth = scope.query(':ACQuire:MDEPth?')[:-1]
-        sample_rate = scope.query(':ACQuire:SRATe?')[:-1]
+        mem_depth = scope.query(':ACQuire:MDEPth?')
+        sample_rate = scope.query(':ACQuire:SRATe?')
 
         scope.write(':WAV:SOUR CHAN'+str(channel))
 
         # Get time scale and offset
-        timescale = float(scope.query(':TIM:SCAL?')[:-1])
-        timeoffset = float(scope.query(':TIM:OFFS?')[:-1])
+        timescale = float(scope.query(':TIM:SCAL?'))
+        timeoffset = float(scope.query(':TIM:OFFS?'))
         # Get voltage scale and offset
-        voltscale = float(scope.query(':CHAN'+str(channel)+':SCAL?')[:-1])
-        voltoffset = float(scope.query(':CHAN'+str(channel)+':OFFS?')[:-1])
+        voltscale = float(scope.query(':CHAN'+str(channel)+':SCAL?'))
+        voltoffset = float(scope.query(':CHAN'+str(channel)+':OFFS?'))
         
-        y_inc = float(scope.query(':WAVeform:YINCrement?')[:-1])
-        y_ref = float(scope.query(':WAVeform:YREFerence?')[:-1])
-        y_ori = float(scope.query(':WAVeform:YORigin?')[:-1])
+        y_inc = float(scope.query(':WAVeform:YINCrement?'))
+        y_ref = float(scope.query(':WAVeform:YREFerence?'))
+        y_ori = float(scope.query(':WAVeform:YORigin?'))
         
-        x_inc = float(scope.query(':WAVeform:XINCrement?')[:-1])
-        x_ref = float(scope.query(':WAVeform:XREFerence?')[:-1])
-        x_ori = float(scope.query(':WAVeform:XORigin?')[:-1])
+        x_inc = float(scope.query(':WAVeform:XINCrement?'))
+        x_ref = float(scope.query(':WAVeform:XREFerence?'))
+        x_ori = float(scope.query(':WAVeform:XORigin?'))
         
-        print(scope.query(':WAVeform:PREamble?'))
+        #print(scope.query(':WAVeform:PREamble?'))
         scope.write(':WAVeform:FORMat BYTE')
         
         #scope.write(':WAVeform:MODE RAW')
         scope.write(':WAVeform:MODE NORMAL')
         #rawdata = scope.query(':WAV:DATA? CHAN'+str(channel)+'\n').encode('ascii')[10:-1]
         scope.write(':WAV:DATA? CHAN'+str(channel)) #Request the data
+        #rawdata = numpy.ones(1210)
         rawdata = scope.read_raw() #Read the block of data
         head = rawdata[:11]
         rawdata = rawdata[11:-1] #Drop the heading
         #print(rawdata[:20])
-        print(head)
+        #print(head)
 
         data_size = len(rawdata)
 
         scope.write(':RUN')
         scope.write(':KEY:FORCE')
-        scope.close()
         
         print('Data size:', data_size, "Sample rate:", sample_rate, "Mem. depth", mem_depth)
 
         
 
     data = numpy.frombuffer(rawdata, 'B').astype(float)
-    #print(data[0:20])
     #print(data.dtype)
-    #print(y_ori,y_ref,y_inc)
-    data = (data - y_ori - y_ref) * y_inc
+    #print('raw data:',data[0:20])
     
-    # dt = :TIM:SCAL?/50
-    # y_offs = :OFFS?
-    # y_inc = :TIM:SCALE?/25
-    # y_ref = 125
-    # y_hex = data
-    #data = (y_ref - y_hex)*y_inc - y_offs
-    print(data[0:20])
-    print(((data[0:20]*-1 + 255) - 130.0 - voltoffset/voltscale*25) / 25 * voltscale)
-
-    # Walk through the data, and map it to actual voltages
-    # This mapping is from Cibo Mahto
-    # First invert the data
+    # Map data to actual voltages (from Cibo Mahto), first invert the data
     #data = data * -1 + 255
-
     # Now, we know from experimentation that the scope display range is actually
     # 30-229.  So shift by 130 - the voltage offset in counts, then scale to
     # get the actual voltage.
     #data = (data - 130.0 - voltoffset/voltscale*25) / 25 * voltscale
+    #print('Shirriff:',((data[0:20]*-1 + 255) - 130.0 - voltoffset/voltscale*25) / 25 * voltscale)
+
+    dt = timescale/50.
+    #y_offs = voltoffset
+    #y_inc = voltscale/25.
+    #y_ref = 125.
+    #y_hex = data
+    #data = (y_ref - y_hex)*y_inc - y_offs
+    #print('labview: ',(125. - data[0:20])*voltscale/25. - voltoffset)
+    #print('offset:',voltoffset,'reference:',y_ref,'increment:',y_inc)
+    
+    data = (data - y_ori - y_ref) * y_inc
+    #print('origin:',y_ori,'reference:',y_ref,'increment:',y_inc)
+    #print('manual: ',data[0:20])
 
     # Now, generate a time axis.
     time = numpy.linspace(timeoffset - 6 * timescale, 
