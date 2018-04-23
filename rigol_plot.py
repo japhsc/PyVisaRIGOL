b062fad846393a6f13e9a13590c24fdd3fb87803
Jan-Philipp Schröder <jan.philipp.schroeder@physik.uni-freiburg.de>
2018-04-23 13:55:25 +0200

    (NI-version) -WAIT for TRG, -add simultaneous query of up to 4 channels (dict), -fix key lock, -clean up code

--- rigol_plot.py
+++ rigol_plot.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 """
+Current version by Jan-Philipp Schoeder
+
+Major improvements: Yanis Taege and Fritz Bayer
+
 Inspired by pklaus/rigol-plot.py, forked from shirriff/rigol-plot.py
 https://gist.github.com/pklaus/7e4cbac1009b668eafab
 
@@ -14,6 +18,7 @@ by Cibo Mahto.
 import numpy
 import visa
 import os
+import time as tme
 
 class ResourceManager(object):
     def __init__(self, **kwargs):
@@ -32,31 +37,45 @@ class ResourceManager(object):
 class Instrument(object):
     def __init__(self, rm, resource_name, **kwargs):
         self.inst = rm.open_resource(resource_name, **kwargs)
-        #self.inst.encoding =  "latin-1"
+        self.query_delay = self.inst.query_delay
     def __enter__(self):
         #return(self.inst)
         return(self)
     def __exit__(self, exc_type, exc, exc_tb):
         self.inst.close()
     
-    def query(self,command):
-        #return(self.inst.query(command))
-        #return(self.inst.query_binary_values(command, datatype='b', is_big_endian=True))
-        self.write(command) #Request the data
-        rawdata = self.read_raw()
-        data = rawdata
-        while (data[-1] is not 10):
-            data =  data[:-1]
-        while (data[-1] is 10):
-            data =  data[:-1]
-        #print(data)
-        return(data.decode("utf-8"))
-        
-    def read_raw(self):
-        return(self.inst.read_raw()) #Read the block of data
+    def query(self,message):
+        data = self.inst.query(message)[:-1]
+        #ret = self.write(message)
+        #if self.query_delay > 0.0:
+        #    tme.sleep(self.query_delay)
+        #data = self.inst.read()[:-1]
+        #if not ret[1]==0:
+        #    err = self.inst.query('SYST:ERR?')
+        #    print('QUERY: "%s" "%s"' % (message, err))
+        #    print(':'.join(str(ord(x)) for x in data))
+        return(data)
     
-    def write(self,command):
-        return(self.inst.write(command))
+    def query_raw(self,message):
+        #return(self.inst.query_binary_values(command, datatype='b', is_big_endian=True))
+        self.write(message) #Request the data
+        rawdata = self.read_raw() #Read the block of data
+        head = rawdata[:11]
+        rawdata = rawdata[11:-1] #Drop the heading
+        return head,rawdata
+    
+    def read_raw(self):
+        return(self.inst.read_raw()) #Read the block ofprint data
+    
+    def write(self,message):
+        ret = self.inst.write(message)
+        
+        #err = self.inst.query('SYST:ERR?')
+        #print('WRITE: "%s" "%s"' % (message, err))
+        
+        #if not ret[1]==0:
+        #    print('WRITE: "%s"' % message, ret)
+        return(ret)
 
 
 def list_device():
@@ -66,14 +85,12 @@ def list_device():
         rm = visa.ResourceManager('@ni')
     else:
         rm = visa.ResourceManager()
-
-    # Get the USB device, e.g. 'USB0::0x1AB1::0x0588::DS1ED141904883'
     instruments = rm.list_resources()
     usb = list(filter(lambda x: 'USB' in x, instruments))
     return usb
 
 
-def read(adr,channel=1):
+def read(adr, channels=[1]):
     if os.name == 'posix':
         rm = visa.ResourceManager('@py')
     elif os.name == 'nt':
@@ -81,93 +98,82 @@ def read(adr,channel=1):
     else:
         rm = visa.ResourceManager()
         
-    with Instrument(rm, adr, timeout=2000, chunk_size=1024000) as scope:
-    #with Instrument(rm, adr) as scope:
-        #print(scope.query("*IDN?"))
+    #with Instrument(rm, adr, timeout=2000, chunk_size=1024000) as scope:
+    with Instrument(rm, adr) as scope:
+        #print(scope.query('*IDN?'))
         
-        scope.write(':KEY:LOCK')
-        scope.write(':RUN')
-        scope.write(':ACQuire:MDEPth 600000')
-        #scope.write(':ACQuire:MDEPth '+mem_depth+'\n')
-        #scope.write(':CHANnel'+str(channel)+':COUPling '+ch_cpl_mode+'\n')
+        scope.write(':SYSTem:LOCKed 1')
+        #scope.write(':RUN')
         
-        # Record data of a single trigger
-        scope.write(':SINGle')
-        # Grab the raw data from channel 1
-        #scope.write(u':STOP\n')
-        
+        #scope.write(':ACQuire:MDEPth 600000')
         mem_depth = scope.query(':ACQuire:MDEPth?')
         sample_rate = scope.query(':ACQuire:SRATe?')
-
-        scope.write(':WAV:SOUR CHAN'+str(channel))
+        #scope.write(':CHANnel'+str(channel)+':COUPling '+ch_cpl_mode)
+        scope.write(':SINGle') 
+        
+        trigger = 'WAIT'
+        while trigger.find('STOP') < 0:
+            trigger = scope.query(":TRIGger:STATus?")
+            tme.sleep(0.01)
 
         # Get time scale and offset
         timescale = float(scope.query(':TIM:SCAL?'))
         timeoffset = float(scope.query(':TIM:OFFS?'))
-        # Get voltage scale and offset
-        voltscale = float(scope.query(':CHAN'+str(channel)+':SCAL?'))
-        voltoffset = float(scope.query(':CHAN'+str(channel)+':OFFS?'))
+        print('timescale %.3f \t timeoffset %.3f' % (timescale, timeoffset))
+
         
-        y_inc = float(scope.query(':WAVeform:YINCrement?'))
-        y_ref = float(scope.query(':WAVeform:YREFerence?'))
-        y_ori = float(scope.query(':WAVeform:YORigin?'))
-        
-        x_inc = float(scope.query(':WAVeform:XINCrement?'))
-        x_ref = float(scope.query(':WAVeform:XREFerence?'))
-        x_ori = float(scope.query(':WAVeform:XORigin?'))
+
         
         #print(scope.query(':WAVeform:PREamble?'))
         scope.write(':WAVeform:FORMat BYTE')
-        
         #scope.write(':WAVeform:MODE RAW')
         scope.write(':WAVeform:MODE NORMAL')
-        #rawdata = scope.query(':WAV:DATA? CHAN'+str(channel)+'\n').encode('ascii')[10:-1]
-        scope.write(':WAV:DATA? CHAN'+str(channel)) #Request the data
-        #rawdata = numpy.ones(1210)
-        rawdata = scope.read_raw() #Read the block of data
-        head = rawdata[:11]
-        rawdata = rawdata[11:-1] #Drop the heading
-        #print(rawdata[:20])
-        #print(head)
-
-        data_size = len(rawdata)
-
-        scope.write(':RUN')
-        scope.write(':KEY:FORCE')
         
-        print('Data size:', data_size, "Sample rate:", sample_rate, "Mem. depth", mem_depth)
-
+        # Record data of a single trigger
+        #scope.write(':STOP')
+        rawdata = dict();
+        for channel in channels:
+            scope.write(':WAV:SOUR CHAN'+str(channel))
+            
+            cpl = scope.query(':CHANnel'+str(channel)+':COUPling?')
+            
+            #x_inc = float(scope.query(':WAVeform:XINCrement?'))
+            #x_ref = float(scope.query(':WAVeform:XREFerence?'))
+            #x_ori = float(scope.query(':WAVeform:XORigin?'))
+            
+            # Get voltage scale and offset
+            #voltscale = float(scope.query(':CHAN'+str(channel)+':SCAL?'))
+            #voltoffset = float(scope.query(':CHAN'+str(channel)+':OFFS?'))
+            y_inc = float(scope.query(':WAVeform:YINCrement?'))
+            y_ref = float(scope.query(':WAVeform:YREFerence?'))
+            y_ori = float(scope.query(':WAVeform:YORigin?'))
+            print('y_inc = %.2f \t y_ref = %.2f \t y_ori = %.2f' % (y_inc, y_ref, y_ori))
+            
+            _,rawdata['channel%d'%channel] = scope.query_raw(':WAV:DATA? CHAN'+str(channel))
+            data_size = len(rawdata['channel%d'%channel])
+            print('CH%d(%s): data size: %d; sampl.rate: %s; Mem.depth: %s' % (channel,cpl,data_size,sample_rate,mem_depth))
+        #tme.sleep(0.4)
+        scope.write(':RUN') # WHILE OPC??
+        # YANIS: MOVED TO 139 ## _,rawdata = scope.query_raw(':WAV:DATA? CHAN'+str(channel))
+        #tme.sleep(2.)
         
-
-    data = numpy.frombuffer(rawdata, 'B').astype(float)
-    #print(data.dtype)
-    #print('raw data:',data[0:20])
+        
+        scope.write(':SYSTem:LOCKed 0')
+        
     
-    # Map data to actual voltages (from Cibo Mahto), first invert the data
-    #data = data * -1 + 255
-    # Now, we know from experimentation that the scope display range is actually
-    # 30-229.  So shift by 130 - the voltage offset in counts, then scale to
-    # get the actual voltage.
-    #data = (data - 130.0 - voltoffset/voltscale*25) / 25 * voltscale
-    #print('Shirriff:',((data[0:20]*-1 + 255) - 130.0 - voltoffset/voltscale*25) / 25 * voltscale)
-
-    dt = timescale/50.
-    #y_offs = voltoffset
-    #y_inc = voltscale/25.
-    #y_ref = 125.
-    #y_hex = data
-    #data = (y_ref - y_hex)*y_inc - y_offs
-    #print('labview: ',(125. - data[0:20])*voltscale/25. - voltoffset)
-    #print('offset:',voltoffset,'reference:',y_ref,'increment:',y_inc)
-    
+    data = numpy.zeros(shape = (len(channels), data_size));
+    for idc, channel in enumerate(channels):
+        data[idc] = numpy.frombuffer(rawdata['channel%d'%channel], 'B').astype(float)
     data = (data - y_ori - y_ref) * y_inc
-    #print('origin:',y_ori,'reference:',y_ref,'increment:',y_inc)
-    #print('manual: ',data[0:20])
 
-    # Now, generate a time axis.
+    # Generate a time axis
+    if len(data)>0.:
+        dt = timescale*12./float(len(data))
+    else:
+        dt = 0.
     time = numpy.linspace(timeoffset - 6 * timescale, 
                           timeoffset + 6 * timescale, 
-                          num=len(data))
-    return time, data
+                          num=data_size)
+    return time, data, dt
 
 
